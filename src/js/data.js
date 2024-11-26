5cbbd3f6f1541f4d267f311d1c5c0bd343b31915
sayuragii <utsusemi@sayuragi.com>
2024-11-25 23:15:01 -0500

    Update data.js

--- src/js/data.js
+++ src/js/data.js
@@ -20,4 +20,4 @@ let dataSetVersion = '';
 /**
  * Image root, will be appended to the start of every image URL.
  */
-const imageRoot = 'https://file.garden/Zi6mdroOThYMNcLy/MDRM%20SORTER/';
+const imageRoot = '/chars/';
